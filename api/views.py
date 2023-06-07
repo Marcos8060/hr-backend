fd2fda088afe4b3968852dcb67de6be16aa3459f
MarcosDMT <marcosgav80@gmail.com>
2023-06-07 07:10:21 +0300

    added register view and its associated serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        reg_serializer = UserSerializer(data=request.data)
+        if reg_serializer.is_valid():
+            new_user = reg_serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
