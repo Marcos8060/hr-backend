4ff78acc0f2495c917cf4761dca869d7e8a8f9ec
MarcosDMT <marcosgav80@gmail.com>
2023-06-29 23:05:57 +0300

    made minor changes

--- api/models.py
+++ api/models.py
@@ -5,13 +5,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name=models.CharField(max_length=200)
-    email=models.EmailField(max_length=240,unique=True)
-    password=models.CharField(max_length=200)
-    username=models.CharField(max_length=200)
 
-    # set email as default
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+
+    # AbstractUser automatically sets the username,email and password
+
+    
 
 
 class Project(models.Model):
--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from .models import User,Project,Todos
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password',]
+        fields = ['id', 'username', 'email', 'password',]
         # eliminate the password from being return upon user registration.
         extra_kwargs = {
             'password' : { 'write_only': True}
